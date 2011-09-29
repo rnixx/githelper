e7eb18c99fb069ffacaca1aae59098f51a771d58
Robert Niederreiter <rnix@squarewave.at>
2011-09-29 14:21:50 +0200

    add backup flag

--- igitt.py
+++ igitt.py
@@ -16,15 +16,13 @@ For cloning repositories, use:
 For updating repositories, use:
     igitt pull [PACKAGE]
 
+For backup of context, use:
+    igitt backup CONTEXT
+
 Install
 -------
 
 Checkout this script and create a symlink in '/usr/local/bin'.
-
-Note
-----
-
-The script always works relative to the directory it was called in.
 """
 
 import os
@@ -52,6 +50,8 @@ def print_usage_and_exit():
     print "    igitt clone CONTEXT [PACKAGE]"
     print "For updating repositories, use:"
     print "    igitt pull [PACKAGE]"
+    print "For backup of context, use:"
+    print "    igitt backup CONTEXT"
     sys.exit(0)
 
 
@@ -107,6 +107,27 @@ def perform_pull(package):
         os.chdir('..')
 
 
+def perform_backup(context):
+    if not context in os.listdir('.'):
+        os.mkdir(context)
+    os.chdir(context)
+    contents = os.listdir('.')
+    data = query_repos(context)
+    base_uri = 'git@github.com:%s/%s.git'
+    for repo in data['repositories']:
+        name = repo['name']
+        fs_name = '%s.git' % name
+        if fs_name in contents:
+            os.chdir(fs_name)
+            cmd = ['git', 'fetch', 'origin']
+            subprocess.call(cmd)
+            os.chdir('..')
+        else:
+            uri = base_uri % (context, name)
+            cmd = ['git', 'clone', '--bare', '--mirror', uri]
+            subprocess.call(cmd)
+
+
 if __name__ == '__main__':
     args = sys.argv
     if len(args) < 2:
@@ -114,7 +135,7 @@ if __name__ == '__main__':
         print_usage_and_exit()
     
     action = args[1]
-    if action not in ['clone', 'pull']:
+    if action not in ['clone', 'pull', 'backup']:
         print "Invalid action '%s'" % action
         print_usage_and_exit()
     
@@ -134,6 +155,14 @@ if __name__ == '__main__':
         if len(args) > 2:
             package = args[2]
         perform_pull(package)
+        sys.exit(0)
+    
+    if action == 'backup':
+        if len(args) != 3:
+            print "Need context to perform backup"
+            print_usage_and_exit()
+        context = args[2]
+        perform_backup(context)
         sys.exit(0)
     
     print "Invalid action '%s'" % action
